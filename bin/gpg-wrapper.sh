a862708c0cc122f010ad764d927df065e09a179f
Matthias Mintert <matthias@mintert.net>
2023-11-13 14:14:47 +0100

    fixes rc

--- bin/gpg-wrapper.sh
+++ bin/gpg-wrapper.sh
@@ -2,15 +2,4 @@
 
 VAULT_PW_FILENAME=".vaultpw"
 gpg --quiet --batch --use-agent --decrypt $VAULT_PW_FILENAME 2>/dev/null
-#if
-#  command -v xclip >/dev/null 2>&1
-#then
-#  gpg --quiet --batch --use-agent --decrypt $VAULT_PW_FILENAME 2>/dev/null |  tee | xclip
-#elif
-#  command -v pbcopy >/dev/null 2>&1
-#then
-#  gpg --quiet --batch --use-agent --decrypt $VAULT_PW_FILENAME 2>/dev/null | tee | pbcopy
-#else
-#  echo "This script requires xclip or pbcopy installed"
-#fi
-
+exit 0
